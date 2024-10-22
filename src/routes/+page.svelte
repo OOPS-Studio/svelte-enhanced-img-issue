6d0d0c73990cce9a1978ff813a7b1e0fa301409a
OOPS! Studio! <gh1@seattle1.com>
2024-10-22 02:18:29 -0700

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	console.log("JavaScript loaded");
+</script>
+
+<enhanced:img src="$lib/assets/cookies1.avif" width="200"></enhanced:img>
+<enhanced:img src="$lib/assets/cookies2.jpg" width="200"></enhanced:img>
+<enhanced:img src="$lib/assets/cookies3.jpg" width="200"></enhanced:img>
+<enhanced:img src="$lib/assets/kitten1.jpg" width="200"></enhanced:img>
+<enhanced:img src="$lib/assets/kitten2.webp" width="200"></enhanced:img>
+<enhanced:img src="$lib/assets/kitten3.jpg" width="200"></enhanced:img>
